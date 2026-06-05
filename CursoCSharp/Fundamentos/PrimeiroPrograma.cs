9ad9f8c6d2181358a5daed81481815999fdf56ed
Lucas Silva <lucasdsg31@outlook.com>
2026-06-05 17:01:23 -0300

    Adicionar arquivos de projeto.

---
+++ CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    internal class PrimeiroPrograma {
+        public static void Executar() {
+            Console.Write("Primeiro ");
+            Console.WriteLine("Programa");
+            Console.WriteLine("Terminou!");
+
+        }
+    }
+}

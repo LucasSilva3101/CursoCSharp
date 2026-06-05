c341400f347ba6db770b50ca53b9f248e10181fe
Lucas Silva <lucasdsg31@outlook.com>
2026-06-05 18:00:32 -0300

    Add class Comentarios.cs

---
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CursoCSharp.Fundamentos {
+    public class Comentarios {
+        public static void Executar() {
+            Console.WriteLine("Código claro é sempre melhor!");
+            Console.WriteLine("O C# tem o XML Comments");
+
+            /*
+             * Comentário de múltiplas linhas
+             */
+
+            // Comentário de uma linha
+
+            // CTRL + K + C -> Comentar
+            // CTRL + K + U -> Descomentar
+            // Console.WriteLine("1");
+            // Console.WriteLine("2");
+        }
+    }
+}
